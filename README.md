ef9a394e39f20f29c6a57fe253b18eefba00dd2b
Baduit <simon.bertho@epitech.eu>
2018-09-30 16:27:52 +0200

    First Commit.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Timer
+
+## Description
 A simple C++ header-only library to use cross-platform timer functionnalities using only the standard library.
+
+## How to use it?
+There is 2 types of class in this library:
+* the __clocks__
+* the __timers__
+
+### The clocks
+A clock allow you to have a duration between now and the last reset of the clock.
+Each clock have a method to have this duration in the format of the std::chrono::system_clock, you can also have this value in any duration defined with std::chrono::duration or as the number of nanoseconds.
+
+There is 2 clocks, __SimpleClock__ with only this functionnalities, and __AdvancedClock__ which can be paused.
+
+In the file __src/main.cpp__ you can see examples to see how to use them.
+
+### The timers
+A timer allow you to start an action at the end of a duration.
+
+If you want to run a function asynchronously in 3 seconds automatically while the main thread is doing something else, a __ThreadTimer__ is what you need.
+
+If you want to run a function asynchronously every 3 seconds while the main thread is doing something else, a __LoopThreadTimer__ is what you need.
+
+In the file __src/main.cpp__ you can see examples to see how to use them.
+
+## How to install it?
+It is a header only library, so you just need to have the header __Timer.hpp__ in your path.
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include "Timer.hpp"
+
+using namespace Timer;
+using namespace std::chrono_literals;
+
+uint64_t nanoToMilli(uint64_t nano)
+{
+	return nano / 1000000;
+}
+
+int	main()
+{
+/* 	std::cout << "SimpleClock" << std::endl;
+	SimpleClock sc;
+	std::this_thread::sleep_for(1s);
+	std::cout << nanoToMilli(sc.getTimeNanoCount()) << " milliseconds" << std::endl;
+	sc.reset();
+	std::cout << nanoToMilli(sc.getTimeNanoCount()) << " milliseconds" << std::endl;
+	std::cout << "End" << std::endl << std::endl; */
+
+	std::cout << "AdvancedClock" << std::endl;
+	AdvancedClock ac;
+	std::this_thread::sleep_for(1s);
+	std::cout << nanoToMilli(ac.getTimeNanoCount()) << " milliseconds" << std::endl;
+	ac.pause();
+	std::this_thread::sleep_for(1s);
+	std::cout << nanoToMilli(ac.getTimeNanoCount()) << " milliseconds" << std::endl;
+	ac.start();
+	std::this_thread::sleep_for(1s);
+	std::cout << nanoToMilli(ac.getTimeNanoCount()) << " milliseconds" << std::endl;
+	std::cout << "End" << std::endl << std::endl;
+/* 
+	std::cout << "ThreadTimer" << std::endl;
+	ThreadTimer tt;
+	auto future_result = tt.start(2s,
+		[]()
+		{
+			std::cout << "At the end of the world;" << std::endl;
+			return 5;
+		}
+	);
+	auto waited_result = future_result.get();
+	std::cout << waited_result << std::endl;
+	ThreadTimer ttt(1s,
+		[]()
+		{
+			std::cout << "Hello from the other side." << std::endl;
+		}
+	);
+	std::this_thread::sleep_for(1.2s);
+	std::cout << "End" << std::endl << std::endl;
+
+	std::cout << "LoopThreadTimer" << std::endl;
+	LoopThreadTimer ltt(0.3s,
+		[]()
+		{
+			std::cout << "At the end of the world;" << std::endl;
+		}
+	);
+	std::this_thread::sleep_for(1s);
+	std::cout << "End" << std::endl;
+ */
+	return 0;
+}
