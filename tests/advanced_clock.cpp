9716f4c70fcb03eb53480309ef91993e46f6c0ac
Simon Bertho <simon.bertho@epitech.eu>
2019-07-13 18:34:44 +0200

    Clocks TU

--- tests/advanced_clock.cpp
+++ tests/advanced_clock.cpp
@@ -1,6 +1,73 @@
+#include <cassert>
+#include <thread>
+
 #include "Timer.hpp"
+
+using namespace std::chrono_literals;
+
+
+// Helper(s)
+namespace
+{
+
+int64_t nanoToMilli(int64_t nano)
+{
+	return nano / 1000000;
+}
+
+}
+
+// Tests
+
+void basic_usage()
+{
+
+	Timer::AdvancedClock ac;
+	std::this_thread::sleep_for(10ms);
+	assert(nanoToMilli(ac.getTimeNanoCount()) == 10);
+
+	ac.pause();
+	std::this_thread::sleep_for(10ms);
+	assert(ac.getDuractionAs<std::chrono::milliseconds>().count() == 10);
+
+	ac.start();
+	std::this_thread::sleep_for(10ms);
+	assert(ac.getDuractionAs<std::chrono::milliseconds>().count() == 20);
+
+	ac.pause();
+	std::this_thread::sleep_for(10ms);
+	assert(ac.getDuractionAs<std::chrono::milliseconds>().count() == 20);
+	ac.start();
+	assert(ac.getDuractionAs<std::chrono::milliseconds>().count() == 20);
+}
+
+void copy()
+{
+	{
+		Timer::AdvancedClock ac1;
+		std::this_thread::sleep_for(20ms);
+		ac1.pause();
+		std::this_thread::sleep_for(20ms);
+
+		Timer::AdvancedClock ac2(ac1);
+		assert(ac1.getDuractionAs<std::chrono::milliseconds>().count() == ac2.getDuractionAs<std::chrono::milliseconds>().count());
+	}
+
+	{
+		Timer::AdvancedClock ac1;
+		std::this_thread::sleep_for(20ms);
+
+		Timer::AdvancedClock ac2;
+		ac2.pause();
+		std::this_thread::sleep_for(20ms);
+		ac2 = ac1;
+		assert(ac1.getDuractionAs<std::chrono::milliseconds>().count() == ac2.getDuractionAs<std::chrono::milliseconds>().count());
+	}
+	
+}
 
 int main()
 {
-	
+	basic_usage();
+	copy();
 }
--- tests/simple_clock.cpp
+++ tests/simple_clock.cpp
@@ -1,6 +1,59 @@
+#include <cassert>
+#include <thread>
+
 #include "Timer.hpp"
+
+using namespace std::chrono_literals;
+
+
+// Helper(s)
+namespace
+{
+
+int64_t nanoToMilli(int64_t nano)
+{
+	return nano / 1000000;
+}
+
+}
+
+// Tests
+
+void basic_usage()
+{
+
+	Timer::SimpleClock sc;
+	std::this_thread::sleep_for(10ms);
+	assert(nanoToMilli(sc.getTimeNanoCount()) == 10);
+
+	sc.reset();
+	assert(sc.getDuractionAs<std::chrono::milliseconds>().count() == 0);
+}
+
+void copy()
+{
+	{
+		Timer::SimpleClock sc1;
+		std::this_thread::sleep_for(20ms);
+
+		Timer::SimpleClock sc2(sc1);
+		assert(sc1.getDuractionAs<std::chrono::milliseconds>().count() == sc2.getDuractionAs<std::chrono::milliseconds>().count());
+	}
+
+	{
+		Timer::SimpleClock sc1;
+		std::this_thread::sleep_for(20ms);
+
+		Timer::SimpleClock sc2;
+		std::this_thread::sleep_for(20ms);
+		sc2 = sc1;
+		assert(sc1.getDuractionAs<std::chrono::milliseconds>().count() == sc2.getDuractionAs<std::chrono::milliseconds>().count());
+	}
+	
+}
 
 int main()
 {
-	
+	basic_usage();
+	copy();
 }
