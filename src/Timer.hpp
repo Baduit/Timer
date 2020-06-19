19f98db897b66b0a672e1d9780ae3fa9eade803c
Baduit <simon.bertho@epitech.eu>
2020-06-19 19:39:41 +0200

    Add the Poller

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -173,6 +173,78 @@ class Sleeper
 		std::condition_variable	_cv;
 };
 
+/*
+** Execute an action at the end of the choosed duration in a loop in the current thread
+** For the moment it can't be paused.
+*/
+class Poller
+{
+	public:
+		enum class LoopStatus
+		{
+			CONTINUE,
+			STOP
+		};
+
+	public:
+		Poller() = default;
+
+		Poller(const Poller&) = delete;
+		Poller& operator=(const Poller&) = delete;
+
+		Poller(Poller&&) = delete;
+		Poller& operator=(Poller&&) = delete;
+
+		template<typename D, typename Cb>
+		Poller(const D& duration, Cb&& cb)
+		{
+			start(duration, std::forward<Cb>(cb));
+		}
+
+		virtual ~Poller()
+		{
+			stop();
+		}
+
+		template<typename D, typename Cb>
+		void	operator()(const D& duration, Cb&& cb)
+		{
+			start(duration, std::forward<Cb>(cb));
+		}
+
+		template<typename D, typename Cb>
+		void	start(const D& duration, Cb&& cb)
+		{
+			while (!_stop)
+			{
+				_sleeper(duration);
+				if (!_stop)
+				{
+					using CbReturnType = std::invoke_result_t<Cb>;
+					if constexpr (std::is_same_v<CbReturnType, LoopStatus>)
+					{
+						if (cb() == LoopStatus::STOP)
+							stop();
+					}
+					else
+					{
+						cb();
+					}
+				}
+			}
+		}
+
+		void	stop()
+		{
+			_stop = true;
+			_sleeper.cancel_all();
+		}
+
+	private:
+		std::atomic<bool>	_stop { false };
+		Sleeper				_sleeper;
+};
+
 
 enum class ActionOnDtor
 {
@@ -181,7 +253,7 @@ enum class ActionOnDtor
 };
 
 /*
-** Execute an action at the end of the choosed duration.
+** Execute an action at the end of the choosed duration in an other thread.
 ** For the moment it can't be paused.
 */
 class ThreadTimer
@@ -246,7 +318,7 @@ class ThreadTimer
 };
 
 /*
-** Execute an action at the end of the choosed duration in a loop.
+** Execute an action at the end of the choosed duration in a loop in an other thread.
 ** For the moment it can't be paused.
 */
 class LoopThreadTimer
